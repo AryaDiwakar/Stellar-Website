8b6f28b73f761548c994221669835469ad7bb1ee
Prathmesh Gahukar <prathmeshgahukar001@gmail.com>
2024-07-01 21:36:53 +0530

    Second Push

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import './Main.css';
-import sun from '../assests/Sun.png'
-import planet from '../assests/planet1.exe'
-// import bgImg from '../assests/starryNight.svg'
+import React from "react";
+import Navbar from "../components/Navbar";
+import "./Main.css";
+import sun from "../assests/Sun.png";
+import earth from "../assests/four.jpg";
+import neptune from "../assests/three.jpg";
+import jupiter from "../assests/two.jpg";
+import saturn from "../assests/one.jpg";
+
 
 export default function Main() {
   return (
-    <div className='mainPage'>
-      <img src={sun} alt="Sun" />
+    <div className="mainPage">
+      <img src={sun} alt="Sun" className="sun" />
+      <section className="planets">
+        <img src={earth} alt="earth" />
+        <img src={jupiter} alt="jupiter" />
+        <img src={neptune} alt="neptune" />
+        <img src={saturn} alt="saturn" />
+      </section>
       <Navbar />
     </div>
-  )
+  );
 }
