9e9669db127c326d95b5af7a3eefa5adbad20751
Pradyun Mondal <134343297+pradyun1611@users.noreply.github.com>
2024-07-08 21:25:19 +0530

    Update Events.jsx

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -22,30 +22,30 @@ export default function Events() {
                         <h5>First slide label</h5>
                         {/* <p>Some representative placeholder content for the first slide.</p> */}
                     </div>
-            </div>
-            <div class="carousel-item">
-                <img src={celestial} class="d-block w-100" alt="Celestial Dive" />
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    {/* <p>Some representative placeholder content for the second slide.</p> */}
+                </div>
+                <div class="carousel-item">
+                    <img src={celestial} class="d-block w-100" alt="Celestial Dive" />
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Second slide label</h5>
+                        {/* <p>Some representative placeholder content for the second slide.</p> */}
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src={cosmic} class="d-block w-100" alt="Cosmic Walk" />
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Third slide label</h5>
+                        {/* <p>Some representative placeholder content for the third slide.</p> */}
+                    </div>
                 </div>
             </div>
-            <div class="carousel-item">
-                <img src={cosmic} class="d-block w-100" alt="Cosmic Walk" />
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    {/* <p>Some representative placeholder content for the third slide.</p> */}
-                </div>
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
     </div>
   )
