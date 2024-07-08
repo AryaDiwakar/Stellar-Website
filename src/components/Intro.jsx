67128aabba5a25ab928e2702412fd66d5aee2b6a
Pradyun Mondal <134343297+pradyun1611@users.noreply.github.com>
2024-07-08 21:21:22 +0530

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ import './Intro.css';
 export default function Intro() {
   return (
     <div className='introComp'>
-        <h1>Among Us</h1>
+        <h1>About Us</h1>
         <p>Astronomy Club - VIT Stellar, where passion meets the cosmos! Founded by astronomy enthusiasts at Vellore Institute of Technology, our club is dedicated to exploring the wonders of the universe and fostering a community of stargazers, scientists, and dreamers. </p>
         <p>Our motive is to ignite curiosity and inspire exploration of the cosmos. Through engaging workshops, star parties, and informative sessions, we aim to make astronomy accessible and enjoyable for everyone, from beginners to seasoned astronomers.</p>
     </div>
