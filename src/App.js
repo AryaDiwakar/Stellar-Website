52ff719de31b130cd63a59b5ff0864d1b2b39e2f
Prathmesh Gahukar <prathmeshgahukar001@gmail.com>
2024-07-10 19:14:57 +0000

    newsletter page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main';
-import About from './pages/About';
-import Whoops404 from './pages/Whoops404';
-import Newsletter from './pages/Newsletter';
-import Gallery from './pages/Gallery';
-import Events from './pages/Events';
-import Background from './components/Background';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import About from "./pages/About";
+import Whoops404 from "./pages/Whoops404";
+import Newsletter from "./pages/Newsletter";
+import Gallery from "./pages/Gallery";
+import Events from "./pages/Events";
+import Background from "./components/Background";
 
 function App() {
   return (
-    <div className='body'>
+    <div className="body">
       <Background />
       <BrowserRouter>
         <Routes>
           <Route index element={<Main />} />
-          <Route path='/home' element={<Main />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/newsletter' element={<Newsletter />} />    
-          <Route path='/gallery' element={<Gallery />} />
-          <Route path='/events' element={<Events />} />
-          <Route path='*' element={<Whoops404 />} />
+          <Route path="/home" element={<Main />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/newsletter" element={<Newsletter />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="*" element={<Whoops404 />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
-};
+}
 
 export default App;
--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,28 +1,14 @@
-import React, { useState } from "react";
-import { Document, Page } from "react-pdf";
+import React from "react";
 import HoriNav from "../components/HoriNav";
 import "./Newsletter.css";
 
 export default function Newsletter() {
-  const [numPages, setNumPages] = useState(6);
-  const [pageNumber, setPageNumber] = useState(1);
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    setNumPages(numPages);
-  }
-
   return (
     <div>
       <HoriNav />
       <div className="newspdf">
         <h1>Newsletter</h1>
-        <div>
-          <Document file="somefile.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-            <Page pageNumber={pageNumber} />
-          </Document>
-          <p>
-            Page {pageNumber} of {numPages}
-          </p>
-        </div>
+        <iframe src="https://drive.google.com/file/d/1Jl8LFXeMdn2myAvvAAmzgBiurdeBHIfi/preview"></iframe>
       </div>
     </div>
   );
