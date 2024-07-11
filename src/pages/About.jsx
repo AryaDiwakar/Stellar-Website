41d5c63d04862cda1e1acf115c9b4a4d8145c84c
Pradyun Mondal <134343297+pradyun1611@users.noreply.github.com>
2024-07-11 17:19:38 +0530

    even more about us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -24,8 +24,8 @@ export default function About() {
             <img src={board} alt='board group picture' />
           </div>
           <div className='desc'>
-            <p>Astronomy Club - VIT Stellar, where passion meets the cosmos! Founded by astronomy enthusiasts at Vellore Institute of Technology, our club is dedicated to exploring the wonders of the universe and fostering a community of stargazers, scientists, and dreamers. </p>
-            <p>Our motive is to ignite curiosity and inspire exploration of the cosmos. Through engaging workshops, star parties, and informative sessions, we aim to make astronomy accessible and enjoyable for everyone, from beginners to seasoned astronomers.</p>
+            <p>Astronomy Club - VIT Stellar, where passion meets the cosmos! Founded by astronomy enthusiasts at Vellore Institute of Technology, our club is dedicated to exploring the wonders of the universe and fostering a community of stargazers, scientists, and dreamers. 
+              Our motive is to ignite curiosity and inspire exploration of the cosmos. Through engaging workshops, star parties, and informative sessions, we aim to make astronomy accessible and enjoyable for everyone, from beginners to seasoned astronomers.</p>
           </div>
         </div>
       </div>
