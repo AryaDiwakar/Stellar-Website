e2b552dd77b6649e1b50d1d3ff9554bb469831ff
Prathmesh Gahukar <prathmeshgahukar001@gmail.com>
2024-07-12 14:44:24 +0530

    not found page :/

--- src/pages/Whoops404.jsx
+++ src/pages/Whoops404.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import HoriNav from "../components/HoriNav";
+import { positionGeometry } from "three/examples/jsm/nodes/Nodes.js";
 
 export default function Whoops404() {
+  const myStyle = {
+    position: "relative",
+    top: "50vh",
+    textAlign: "center",
+    color: "#f8bc04"
+  };
   return (
     <div>
       <HoriNav />
-      Error 404: Not Found
+      <h1 style={myStyle}>Error 404: Not Found</h1>
     </div>
   );
 }
