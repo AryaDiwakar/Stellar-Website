59233c0ef1299069bd6b217df1e36a0e21ce8514
Prathmesh Gahukar <prathmeshgahukar001@gmail.com>
2024-07-12 15:17:57 +0530

    gallery (responsive !mostly)

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,11 +1,52 @@
 import React from "react";
 import HoriNav from "../components/HoriNav";
+import "./Gallery.css";
+import one from "../assets/gallery/one.jpg";
+import two from "../assets/gallery/two.jpg";
+import three from "../assets/gallery/three.jpg";
+import four from "../assets/gallery/four.jpg";
+import five from "../assets/gallery/five.jpg";
 
 export default function Gallery() {
   return (
     <div>
-      {/* Gallery */}
       <HoriNav />
+      <div className="gallerypage">
+        <h1>Gallery</h1>
+        <div className="container galleryBox">
+          <div className="galleryImg">
+            <img src={one} alt="one" />
+          </div>
+          <div className="galleryImg">
+            <img src={two} alt="two" />
+          </div>
+          <div className="galleryImg">
+            <img src={three} alt="three" />
+          </div>
+          <div className="galleryImg">
+            <img src={four} alt="four" />
+          </div>
+          <div className="galleryImg">
+            <img src={five} alt="five" />
+          </div>
+          {/*  */}
+          <div className="galleryImg">
+            <img src={one} alt="one" />
+          </div>
+          <div className="galleryImg">
+            <img src={two} alt="two" />
+          </div>
+          <div className="galleryImg">
+            <img src={three} alt="three" />
+          </div>
+          <div className="galleryImg">
+            <img src={four} alt="four" />
+          </div>
+          <div className="galleryImg">
+            <img src={five} alt="five" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
