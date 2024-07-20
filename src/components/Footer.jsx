65960d3d396b11811aafb39d08aa376e5b3ebdbb
Pradyun Mondal <134343297+pradyun1611@users.noreply.github.com>
2024-07-20 16:25:14 +0530

    more

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,25 +12,27 @@ import { NavLink } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer>
-      <div id="footlogo">VIT - Stellar</div>
-      <div className="connectUs">
-        <a
-          href="https://www.instagram.com/vit_stellar?igsh=MXdiNGI3cDg5YzYxMA=="
-          target="_blank"
-        >
-          <FaInstagram />
-        </a>
-        <a href="https://www.linkedin.com/company/vit-stellar/" target="_blank">
-          <FaLinkedinIn />
-        </a>
-        <a
-          href="https://www.facebook.com/vitstellar?mibextid=ZbWKwL"
-          target="_blank"
-        >
-          <FaFacebook />
-        </a>
-      </div>
-    </footer>
+    <div id="footerContainer">
+      <footer>
+        <div id="footlogo">VIT - Stellar</div>
+        <div className="connectUs">
+          <a
+            href="https://www.instagram.com/vit_stellar?igsh=MXdiNGI3cDg5YzYxMA=="
+            target="_blank"
+          >
+            <FaInstagram />
+          </a>
+          <a href="https://www.linkedin.com/company/vit-stellar/" target="_blank">
+            <FaLinkedinIn />
+          </a>
+          <a
+            href="https://www.facebook.com/vitstellar?mibextid=ZbWKwL"
+            target="_blank"
+          >
+            <FaFacebook />
+          </a>
+        </div>
+      </footer>
+    </div>
   );
 }
