3dd6bc9814cd52dc6f9c752f8b2cff3763e4bc84
Prathmesh Gahukar <prathmeshgahukar001@gmail.com>
2024-07-24 23:23:27 +0530

    hgfuigkj

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,8 +5,8 @@ export default function Intro() {
   return (
     <div className='introComp'>
         <h1>About Us</h1>
-        <p>Astronomy Club - VIT Stellar, where passion meets the cosmos! Founded by astronomy enthusiasts at Vellore Institute of Technology, our club is dedicated to exploring the wonders of the universe and fostering a community of stargazers, scientists, and dreamers. </p>
-        <p>Our motive is to ignite curiosity and inspire exploration of the cosmos. Through engaging workshops, star parties, and informative sessions, we aim to make astronomy accessible and enjoyable for everyone, from beginners to seasoned astronomers.</p>
+        <p>Astronomy Club - VIT Stellar, where passion meets the cosmos! Founded by astronomy enthusiasts at Vellore Institute of Technology, our club is dedicated to exploring the wonders of the universe and fostering a community of stargazers, scientists, and dreamers.
+        Our motive is to ignite curiosity and inspire exploration of the cosmos. Through engaging workshops, star parties, and informative sessions, we aim to make astronomy accessible and enjoyable for everyone, from beginners to seasoned astronomers.</p>
     </div>
   )
 }
