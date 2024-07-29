5d8638d56d25daec3925db30a7f8a8ca8c67ce42
Prathmesh Gahukar <prathmeshgahukar001@gmail.com>
2024-07-29 12:59:39 +0530

    newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import './Intro.css';
-import pic1 from '../assets/newsletter/juneNewsLetter (1)_page-0001.jpg'
-import pic2 from '../assets/newsletter/juneNewsLetter (1)_page-0004.jpg'
-import pic3 from '../assets/newsletter/juneNewsLetter (1)_page-0006.jpg'
+import React from "react";
+import "./Intro.css";
+import { NavLink } from "react-router-dom";
+import pic1 from "../assets/newsletter/juneNewsLetter (1)_page-0001.jpg";
+import pic2 from "../assets/newsletter/juneNewsLetter (1)_page-0004.jpg";
+import pic3 from "../assets/newsletter/juneNewsLetter (1)_page-0006.jpg";
 
 export default function Newsletter() {
   return (
-    <div className='nl'>
-        <h1>Newsletter</h1>
-        <img id='pic2' src={pic2} alt = "newsletter1"/>
-        <img id='pic1' src={pic1} alt = "newsletter2"/>
-        <img id='pic3' src={pic3} alt = "newsletter3"/>
+    <div className="nl">
+      <h1>Newsletter</h1>
+      <NavLink to={"/newsletter"} id="pic2">
+        <img  src={pic2} alt="newsletter1" />
+        <p>Jun '24</p>
+      </NavLink>
+      <NavLink to={"/newsletter"} id="pic1">
+        <img  src={pic1} alt="newsletter2" />
+        <p>Jul '24</p>
+      </NavLink>
+      <NavLink to={"/newsletter"} id="pic3">
+        <img  src={pic3} alt="newsletter3" />
+        <p>May '24</p>
+      </NavLink>
     </div>
-  )
+  );
 }
